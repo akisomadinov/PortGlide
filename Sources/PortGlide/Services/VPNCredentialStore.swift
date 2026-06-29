36793db91a3228d7675ba4578a7ea3cc44c0eabf
Azizkhon Isomadinov <isomadinow@gmail.com>
2026-06-29 13:30:55 +0300

    Normalize source formatting

--- Sources/PortGlide/Services/VPNCredentialStore.swift
+++ Sources/PortGlide/Services/VPNCredentialStore.swift
@@ -59,4 +59,3 @@ final class VPNCredentialStore {
         guard addStatus == errSecSuccess else { throw KeychainError.status(addStatus) }
     }
 }
-
--- Sources/PortGlide/Views/VPNCredentialsEditor.swift
+++ Sources/PortGlide/Views/VPNCredentialsEditor.swift
@@ -58,4 +58,3 @@ struct VPNCredentialsEditor: View {
         }
     }
 }
-
